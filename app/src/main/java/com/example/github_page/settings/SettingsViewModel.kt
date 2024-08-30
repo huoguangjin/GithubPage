c68b701df06550210ed83703b6ad25c602cebb73
huoguangjin <352886006@qq.com>
2024-08-30 16:53:55 +0800

    [feat] save settings

--- app/src/main/java/com/example/github_page/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/github_page/settings/SettingsViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.github_page.settings
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
+import androidx.core.content.edit
+import androidx.lifecycle.AndroidViewModel
 
-class SettingsViewModel : ViewModel() {
+class SettingsViewModel(
+    private val app: Application
+) : AndroidViewModel(app) {
+    companion object {
+        private const val SP_NAME = "SP_SETTINGS"
+        private const val SP_KEY_THEME = "SP_THEME"
+    }
+
+    private val sp: SharedPreferences by lazy {
+        app.getSharedPreferences(SP_NAME, Context.MODE_PRIVATE)
+    }
+
     val theme = mutableStateOf(ThemeType.AUTO)
 
     val isDarkTheme: Boolean
@@ -15,8 +30,19 @@ class SettingsViewModel : ViewModel() {
             else -> isSystemInDarkTheme()
         }
 
+    init {
+        readSettings()
+    }
+
+    private fun readSettings() {
+        theme.value = sp.getString(SP_KEY_THEME, ThemeType.AUTO)!!
+    }
+
     fun updateTheme(newTheme: String) {
         theme.value = newTheme
+        sp.edit {
+            putString(SP_KEY_THEME, newTheme)
+        }
     }
 }
 
